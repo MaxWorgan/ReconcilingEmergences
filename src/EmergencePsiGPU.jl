a35a13253bd30e8b095245eec9663f1904d9a9a7
Max Worgan <1410832+MaxWorgan@users.noreply.github.com>
2023-04-16 10:50:47 +0100

    More tidying up

--- src/EmergencePsiGPU.jl
+++ src/EmergencePsiGPU.jl
@@ -5,20 +5,20 @@ using NVTX
 function EmergencePsiGPU(x,v, tau=1)
 
     NVTX.@range "Allocating all the data" begin
-    c_index = [CartesianIndices((1:(size(x,1) -tau), 1:(size(v,2) + tau), i:i)) for i in 1:size(x)[end]] |> cu
-    logdet_rho = CUDA.zeros(Float32, size(x)[end])
-    logdet_s2rho = CUDA.zeros(Float32, size(x)[end])
+        c_index      = [CartesianIndices((1:(size(x,1) -tau), 1:(size(v,2) + tau), i:i)) for i in 1:size(x)[end]] |> cu
+        logdet_rho   = CUDA.zeros(Float32, size(x)[end])
+        logdet_s2rho = CUDA.zeros(Float32, size(x)[end])
 
-    C1 = CUDA.zeros(Float32, size(x,1)-1, size(v,2)+1, size(x)[end])
-    rho1 = CUDA.zeros(Float32, size(C1,2), size(C1,2), size(x)[end])
-    rho2 = CUDA.zeros(Float32, size(C1,2)-1, size(C1,2)-1, size(x)[end])
-    col_means1 = CUDA.zeros(size(x)[end], 11)
-    col_std1 = CUDA.zeros(size(x)[end], 11)
+        C1         = CUDA.zeros(Float32, size(x,1)-1, size(v,2)+1, size(x)[end])
+        rho1       = CUDA.zeros(Float32, size(C1,2), size(C1,2), size(x)[end])
+        rho2       = CUDA.zeros(Float32, size(C1,2)-1, size(C1,2)-1, size(x)[end])
+        col_means1 = CUDA.zeros(size(x)[end], 11)
+        col_std1   = CUDA.zeros(size(x)[end], 11)
 
-    C2 = CUDA.zeros(Float32, size(v,1)-1, size(v,2)*2)
-    rho3 = CUDA.zeros(Float32, size(C2,2), size(C2,2))
-    col_means2 = CUDA.zeros(size(C2)[2])
-    col_std2 = CUDA.zeros(size(C2)[2])
+        C2         = CUDA.zeros(Float32, size(v,1)-1, size(v,2)*2)
+        rho3       = CUDA.zeros(Float32, size(C2,2), size(C2,2))
+        col_means2 = CUDA.zeros(size(C2)[2])
+        col_std2   = CUDA.zeros(size(C2)[2])
     end
 
     NVTX.@range "Computing X MI" begin
@@ -30,6 +30,5 @@ function EmergencePsiGPU(x,v, tau=1)
     end
 
     return v_mi - x_mi
-
 
 end
--- src/ReconcilingEmergences.jl
+++ src/ReconcilingEmergences.jl
@@ -1,6 +1,6 @@
 module ReconcilingEmergences
 
-export EmergencePsi, EmergenceDelta, EmergenceGamma, GaussianMI, DiscreteMI
+export EmergencePsi, EmergenceDelta, EmergenceGamma, GaussianMI, DiscreteMI, EmergencePsiGPU
 # Write your package code here.
 include("GPUUtils.jl")
 include("DiscreteMI.jl")
@@ -8,6 +8,8 @@ include("GaussianMI.jl")
 include("EmergencePsi.jl")
 include("EmergenceDelta.jl")
 include("EmergenceGamma.jl")
+include("EmergencePsiGPU.jl")
+include("GaussianMIGPU.jl")
 
 
 end
